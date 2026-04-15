4e0802489299624d1c251a2adceffa18a312b06b
Andres-Telecom <andres.ugsha@udla.edu.ec>
2026-04-15 09:04:02 -0500

    Enhance Cuenta class with additional methods
    Added methods for setting the account holder and retrieving balance.

--- Ejercicio Bancario/src/Cuenta.java
+++ Ejercicio Bancario/src/Cuenta.java
@@ -1,7 +1,45 @@
-public void depositar(double amount) {
-    if (amount > 0) {
-        saldo += amount;
-    } else {
-        throw new IllegalArgumentException("Amount must be positive");
+import java.sql.SQLOutput;
+
+public class Cuenta {
+    private String numero;
+    private String titular;
+    private float saldo;
+
+    public Cuenta (String numero, String titular, float saldo){
+
+        this.numero=numero;
+        this.titular=titular;
+        this.saldo=saldo;
     }
+
+    public  void setTitular(String titular){
+        this.titular = titular;
+    }
+    public String getTitular(){
+        return titular;
+    }
+    public float getSaldo(){
+        return saldo;
+    }
+
+    public void retirar (float cantidad) {
+        if (cantidad <= 0){
+            System.out.println("no se pueden retirara montos negativos");
+        }else {
+            if(saldo >= cantidad){
+                saldo = saldo - cantidad;
+                System.out.println("Se retiro correctamente");
+                System.out.println("Su nuevo saldo es: "+saldo);
+
+
+            }else{
+                System.out.println("No puede retirar la cantidad");
+                System.out.println("Saldo insuficiengte");
+                System.out.println("Saldo actual: "+getSaldo());
+            }
+        }
+
+    }
+
+
 }
